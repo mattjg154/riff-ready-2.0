a7f32b317e5bfb03149d6210d58a9216ed25a475
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-09-26 18:06:46 +0100

    Set up refresh hook

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,9 +1,37 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
-  useEffect(() => {}, [code]);
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/login", { code })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [code]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/refresh", { refreshToken })
+      .then((res) => {
+        // setAccessToken(res.data.accessToken);
+        // setRefreshToken(res.data.refreshToken);
+        // setExpiresIn(res.data.expiresIn);
+        // window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [refreshToken, expiresIn]);
+  return accessToken;
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log(data.body);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -23,6 +45,10 @@ app.post("/login", (req, res) => {
       });
     })
     .catch(() => {
-      res.sendStatus(400).json({ error: "Failed to authorize with Spotify" });
+      res.sendStatus(400);
     });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
