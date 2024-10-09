aff072fa18596c5b2aa55490171cfc2be92814de
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-10-09 17:04:49 +0100

    changed dotenv to variable

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import dotenv from "dotenv";
-dotenv.config();
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=98615042f4214de7b0970c94a4796730&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const REDIRECT_URI = "https://riff-ready-2-0-client.vercel.app/";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=98615042f4214de7b0970c94a4796730&response_type=code&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
   return (
