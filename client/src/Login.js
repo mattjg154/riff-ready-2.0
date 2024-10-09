edecf6ed3e47b6f7fde3e1a8e941f3a39e590bdf
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-10-09 16:51:55 +0100

    added import for dotenv

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import dotenv from "dotenv";
+dotenv.config();
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=98615042f4214de7b0970c94a4796730&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
