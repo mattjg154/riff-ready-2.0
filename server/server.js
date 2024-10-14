30382f85ba9ef9e25b9d31ec8b551ac3f5528192
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-10-14 15:07:25 +0100

    updated vercel json

--- server/server.js
+++ server/server.js
@@ -7,13 +7,11 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const PORT = 8000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/refresh", (req, res) => {
-  console.log("Refreshing access token");
+app.post("/api/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
@@ -34,9 +32,8 @@ app.post("/refresh", (req, res) => {
     });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   const code = req.body.code;
-  //console.log("Received code:", code);
   const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
@@ -52,15 +49,12 @@ app.post("/login", (req, res) => {
       });
     })
     .catch((err) => {
-      //console.log("Failed to authenticate with Spotify", err);
+      res.sendStatus(400);
     });
 });
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
