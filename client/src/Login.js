4118a0ccf63d5ed2aaadd4327039bf9f62865ee5
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-10-16 16:29:05 +0100

    installed tailwind

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,26 +7,11 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=98615042f4214
 export default function Login() {
   return (
     <>
-      <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-        <div className="h-full lg:flex flex-col items-center justify-center px-4">
-          <div className="text-center space-y-4 pt-16">
-            <h1 className="font-bold text=3xl text-#2E2A47"> Welcome Back!</h1>
-            <p className="text-base text-[#7E8CA0]">
-              Log in to start playing music.
-            </p>
-          </div>
-          <div className="flex item-center justify-center mt-8">
-            <Container className="d-flex justify-content-center align-items-center">
-              <a className="btn btn-success btn-lg" href={AUTH_URL}>
-                Login With Spotify
-              </a>
-            </Container>
-          </div>
-        </div>
-        <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-          <Image src="/logo.svg" alt="logo" height={100} width={100}></Image>
-        </div>
-      </div>
+      <Container className="d-flex tw-bg-gray-500 justify-content-center align-items-center">
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+          Login With Spotify
+        </a>
+      </Container>
     </>
   );
 }
