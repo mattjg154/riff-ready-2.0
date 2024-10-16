213a889317b48c3efd929c08165b660a44eed1a1
Matthew Goulding <mxg183@student.bham.ac.uk>
2024-10-16 16:37:16 +0100

    imported image

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
 const REDIRECT_URI = "https://riff-ready-2-0-client.vercel.app/";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=98615042f4214de7b0970c94a4796730&response_type=code&redirect_uri=https://riff-ready-2-0-client.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
